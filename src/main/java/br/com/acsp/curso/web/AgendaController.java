5ee761243a2b8bef39ba73cf186a5d94d5663913
fabiopedrosa1980 <bito.pedrosa@gmail.com>
2013-08-18 17:00:37 -0300

    ajustes agenda falta terminar

--- src/main/java/br/com/acsp/curso/web/AgendaController.java
+++ src/main/java/br/com/acsp/curso/web/AgendaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import br.com.acsp.curso.domain.Agenda;
 import br.com.acsp.curso.service.AeronaveService;
 import br.com.acsp.curso.service.AgendaService;
+import br.com.acsp.curso.service.AlunoService;
 import br.com.acsp.curso.service.AulaService;
 import br.com.acsp.curso.service.InstrutorService;
 
@@ -24,7 +25,8 @@ import br.com.acsp.curso.service.InstrutorService;
 @Controller
 public class AgendaController extends AbstractController {
 
-    @Autowired private AgendaService agendaService;
+	@Autowired private AlunoService alunoService;
+	@Autowired private AgendaService agendaService;
     @Autowired private AeronaveService aeronaveService;
     @Autowired private InstrutorService instrutorService;
     @Autowired private AulaService aulaService;
@@ -42,28 +44,49 @@ public class AgendaController extends AbstractController {
     @RequestMapping(value = "/agenda", method = RequestMethod.POST)
     public String criaAgendamento(@ModelAttribute("agenda") Agenda agenda) {
         agendaService.salvar(agenda);
-        return "redirect:agendas";
+        return "redirect:/agendas";
     }
 
     @RequestMapping(value = "/agenda", method = RequestMethod.GET)
     public String preparaForm(ModelMap map) {
+    	map.put("listaDeAlunos", alunoService.listarOrdenado());
     	map.put("listaDeAeronaves", aeronaveService.listarOrdenadoPorModelo());
     	map.put("listaDeInstrutores", instrutorService.listarOrdenado());
     	map.put("listaDeAulas", aulaService.listarOrdenado());
-    	
     	return "agenda/formulario";
     }
+    
+    @RequestMapping(value = "/agenda/{id}", method = RequestMethod.GET)
+	public String buscaPorId(@PathVariable("id") Long id, ModelMap modelMap) {
+		modelMap.put("agenda", agendaService.obtemPorId(id));
+		modelMap.put("listaDeAlunos", alunoService.listarOrdenado());
+		modelMap.put("listaDeAeronaves", aeronaveService.listarOrdenadoPorModelo());
+		modelMap.put("listaDeInstrutores", instrutorService.listarOrdenado());
+		modelMap.put("listaDeAulas", aulaService.listarOrdenado());
+ 
+		return "agenda/formulario";
+	}
 
+    
     @RequestMapping(value = "/agenda/{id}", method = RequestMethod.POST)
-    public String atualiza(@PathVariable("id") Long id, @ModelAttribute("agenda") Agenda agenda) {
-        agenda.setId(id);
-        agendaService.alterar(agenda);
-        return "agenda/lista";
-    }
+	public String atualiza(@PathVariable("id") Long id,
+			@ModelAttribute("agenda") Agenda agenda) {
+    	agenda.setId(id);
+    	agendaService.alterar(agenda);
+		return "redirect:/agendas";
+	}
 
     @RequestMapping(value = "/agendas", method = RequestMethod.GET)
     public String lista(ModelMap map) {
-        map.put("listaDeAlunos", agendaService.pesquisarTodos());
+        map.put("listaDeAgendas", agendaService.pesquisarTodos());
         return "agenda/lista";
     }
+    
+	@RequestMapping(value = "/agenda/{id}/apaga", method = RequestMethod.GET)
+	public String exclui(@PathVariable("id") Long id) {
+		agendaService.excluirPorId(id);
+		return "redirect:/agendas";
+	}
+
+    
 }
